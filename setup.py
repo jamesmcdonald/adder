cc3b5703d9895baf5c1c9afdfd848989f88123c8
James McDonald <james@redpill-linpro.com>
2016-10-31 12:02:33 +0100

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='adder',
+        version='0.1',
+        description='A mighty package to add things',
+        url='http://github.com/jamesmcdonald/adder',
+        author='James McDonald',
+        author_email='james@jamesmcdonald.com',
+        license='MIT',
+        packages=['adder'],
+        zip_safe=False)
